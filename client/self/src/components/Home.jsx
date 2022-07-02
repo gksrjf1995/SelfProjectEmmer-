534b6819cffee4262e6bc04ced20aa0e81f3ca6a
gksrjf1995 <gksrjf1995@naver.com>
2022-07-02 11:55:17 +0900

    HOMEFIRSTCOMMIT

--- client/self/src/components/Home.jsx
+++ client/self/src/components/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Navbar from './Navbar'
 import Form  from './Form'
 import Slider from './Slider'
-import Slider2 from './Slider2'
+
 const Home = () => {
   return (
     <div className='mx-96'>
-    <Slider2/>   
+    <Slider/>   
     </div>
        
    
